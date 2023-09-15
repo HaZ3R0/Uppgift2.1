53c3ee04ecbcd28f5eed5a8372e9ce54ecde7331
majo06005 <majo06005@E-PW0234K2>
2023-09-15 12:27:14 +0200

    Add project files.

---
+++ Uppgift2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string bok=(Console.ReadLine());
+            Console.WriteLine("Vad roligt " + bok + " är min favorit bok också");
+            Console.ReadKey();
+        }
+    }
+}
